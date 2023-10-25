72a3d07bdfa601527e906727cfcc68939f017d7c
HernandoLafaurie <lafamori10@gmai.com>
2023-10-25 16:40:37 -0500

    Ruta, Handler y Controller, para buscar productos  por nombre y en caso de que no busques nada devuelve todos los productos, y un middleware que hacia falta para que no se rompan las request en el navegador

--- Api/src/server.js
+++ Api/src/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const routes = require('./routes/index.js');
+const router = require('./routes/index.js');
 const morgan = require("morgan");
 const cors = require("cors");
-const router = require('./routes/index')
 
 const server = express();
-// Para ver nuestras peticiones por consola
-server.use(morgan("dev"));
-// Para detectar la estructura json
+
+server.use(cors()); //Permite que cualquier cliente se conecte 
+
+server.use(morgan("dev")); //Registra tus peticiones en la terminal.
+
 server.use((req, res, next)=> {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
@@ -18,10 +19,10 @@ server.use((req, res, next)=> {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 }); // Para que mi servidor no se rompa al conectar con el browser.
-server.use(express.json());
-// Middleware cors, permite que cualquier cliente se conecte
-server.use(cors());
-// Cualquier solicitud a la ruta raíz del servidor seran manejada por las rutas definidas en routes.
-server.use('/', routes);
+
+server.use(express.json()); //Funcion de parseo que detecta que es un json.
+
+server.use(router); //Cualquier solicitud a la ruta raíz del servidor seran manejada por las rutas definidas en routes.
+
 
 module.exports = server;
