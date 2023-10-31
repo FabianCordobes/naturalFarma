e0a914e08402054f7a3d5f48ddee6c5dd899897e
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2023-10-31 00:40:20 -0300

    crudusercompleto

--- Api/src/controllers/UserController.js
+++ Api/src/controllers/UserController.js
@@ -21,6 +21,43 @@ const newUser = await User.create({
 return newUser;
 };
 
+const deleteUserController = async(id) => await User.destroy({where: {id}});
+
+
+// Esta funcion nos trae los user que tengan una fecha de borrado 
+const getUserDeleteController = async (id) => {
+    const deletedUsers = await User.findAll({
+      where: {
+        deletedAt: {
+          [Op.not]: null, // Filtra usuarios con deletedAt no nulo (usuarios eliminados)
+        },
+      },
+      paranoid: false, // Incluye registros eliminados en la consulta
+    });
+  
+    return deletedUsers;
+  };
+
+
+  const getAllUserControllers = async () => {
+    const allUserDb = await User.findAll();
+    return allUserDb;
+};
+
+ const restoreUserController = async (id)=>{
+        try {
+            await User.restore({
+                where : {id : id}
+            })
+        } catch (error) {
+            throw new Error (error.message)
+        }
+ }
+
 module.exports = {
     createUserController,
+    deleteUserController,
+    getUserDeleteController,
+    getAllUserControllers,
+    restoreUserController,
 }
