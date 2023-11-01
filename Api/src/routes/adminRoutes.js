c44a9fbb405d958c8070cd3b59937e61254b3e64
franquesino <you@example.com>
2023-11-01 19:06:05 -0300

    actualiza

---
+++ Api/src/routes/adminRoutes.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const adminController = require('../controllers/adminController');
+// const adminHandler = require('../handlers/adminHandler');
+
+// const router = express.Router();
+
+// router.get('/admin', adminHandler.isAdmin);
+
+// module.exports = router;
--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,14 +1,18 @@
 const { Router } = require("express");
 const productRouter = require ("../routes/Product")
 const userRouter = require ("../routes/UserRouter")
-const loginRouter = require ("../routes/loginRouter")
+const authRoutes = require('./authRoutes');
+//const adminRoutes = require('./adminRoutes');
+
 
 
 const router = Router();
 
 router.use("/product", productRouter)
 router.use("/user", userRouter)
-router.use("/login", loginRouter)
+router.use("/auth", authRoutes)
+//router.use("/admin", adminRoutes)
+
 
 
 module.exports = router;
