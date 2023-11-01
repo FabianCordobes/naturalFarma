fdd055c41aae1d1b14665c2a3555136a83c80962
HernandoLafaurie <lafamori10@gmai.com>
2023-10-31 21:37:07 -0500

    Slide arreglado, automatico solo con useEffect y useState

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,67 +1,62 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './Home.module.css';
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
 import img1 from '../../assets/oferta1.webp';
 import img2 from '../../assets/oferta2.webp';
 import { FaTruck, FaWhatsapp } from 'react-icons/fa';
 import { BiSolidCreditCardFront } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
-// 
-
 const subcategorias = {
-	Medicinales: ['Subcat1', 'Subcat2', 'Subcat3'],
-	Perfumería: ['Subcat4', 'Subcat5', 'Subcat6'],
-	Accesorios: ['Subcat7', 'Subcat8', 'Subcat9'],
-	Estética: ['Subcat10', 'Subcat11', 'Subcat12'],
+  Medicinales: ['Subcat1', 'Subcat2', 'Subcat3'],
+  Perfumería: ['Subcat4', 'Subcat5', 'Subcat6'],
+  Accesorios: ['Subcat7', 'Subcat8', 'Subcat9'],
+  Estética: ['Subcat10', 'Subcat11', 'Subcat12'],
 };
 
 export default function Home() {
-	const [activeCategory, setActiveCategory] = useState(null);
-	const handleCategoryHover = (category) => {
-		setActiveCategory(category);
-	};
-	const handleCategoryLeave = () => {
-		setActiveCategory(null);
-	};
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	const categories = [
-		{ name: 'Medicinales', path: '/medicinal' },
-		{ name: 'Perfumería', path: '/perfumery' },
-		{ name: 'Accesorios', path: '/accesories' },
-		{ name: 'Estética', path: '/esthetic' },
-	  ];
-	  
-	useEffect(() => {
-		const swiper = new Swiper('.swiper-container', {
-			slidesPerView: 1, // Show only one slide per view
-			loop: true,
-			// autoplay: {
-				// delay: 3000, // Change slide every 3 seconds
-			// },
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-		});
-	}, []);
+  const handleCategoryHover = (category) => {
+    setActiveCategory(category);
+  };
 
-	return (
-		<div className={style.container}>
-			<div className={style.cuerpo}>
-        		<div className={style.botones}>
-          			{categories.map((category) => (
-            			<div key={category.name} onMouseEnter={() => handleCategoryHover(category.name)}>
-              				<Link to={category.path} className={style.link}>
-                		<button className={style.btn}>{category.name}</button>
-              				</Link>
-              			<div className={style.subcategoriasCont}>
-                			{activeCategory === category.name && (
-                  		<div className={style.subcategorias} onMouseLeave={handleCategoryLeave}>
-                    		{subcategorias[category.name].map((subCat) => (
-						<span key={subCat} className={style.subcategoria}>
-                        	{subCat}
+  const handleCategoryLeave = () => {
+    setActiveCategory(null);
+  };
+
+  const categories = [
+    { name: 'Medicinales', path: '/medicinal' },
+    { name: 'Perfumería', path: '/perfumery' },
+    { name: 'Accesorios', path: '/accesories' },
+    { name: 'Estética', path: '/esthetic' },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+    }, 5000);
+
+    return () => {
+      clearInterval(interval); 
+    };
+  }, []);
+
+  return (
+    <div className={style.container}>
+      <div className={style.cuerpo}>
+        <div className={style.botones}>
+          {categories.map((category) => (
+            <div key={category.name} onMouseEnter={() => handleCategoryHover(category.name)}>
+              <Link to={category.path} className={style.link}>
+                <button className={style.btn}>{category.name}</button>
+              </Link>
+              <div className={style.subcategoriasCont}>
+                {activeCategory === category.name && (
+                  <div className={style.subcategorias} onMouseLeave={handleCategoryLeave}>
+                    {subcategorias[category.name].map((subCat) => (
+                      <span key={subCat} className={style.subcategoria}>
+                        {subCat}
                       </span>
                     ))}
                   </div>
@@ -71,40 +66,30 @@ export default function Home() {
           ))}
         </div>
       </div>
-
-			<div className={style.slider}>
-				<div className="swiper-container">
-					<div className="swiper-wrapper">
-						<div className="swiper-slide">
-							<img
-								src={img1}
-								alt="Oferta 1"
-								width={'100%'}
-							/>
-						</div>
-						<div className="swiper-slide">
-							<img
-								src={img2}
-								alt="Oferta 2"
-								width={'100%'}
-							/>
-						</div>
-					</div>
-					<div className="swiper-pagination"></div>
-				</div>
-			</div>
-			<ul className={style.servicios}>
-				<li className={style.servicioItem}>
-					<BiSolidCreditCardFront className={style.icon} />{' '}
-					<span>Hasta 12 cuotas sin interés</span>
-				</li>
-				<li className={style.servicioItem}>
-					<FaWhatsapp className={style.icon} /> <span>Atención personalizada</span>
-				</li>
-				<li className={style.servicioItem}>
-					<FaTruck className={style.icon} /> <span>Calculá el costo de tu envío</span>
-				</li>
-			</ul>
-		</div>
-	);
+      <div className={style.slider}>
+        <div className="slideshow">
+          <div className="slideshowSlider">
+            <div className="slide" style={{ display: currentIndex === 0 ? 'block' : 'none' }}>
+              <img src={img1} alt="Oferta 1" width="100%" />
+            </div>
+            <div className="slide" style={{ display: currentIndex === 1 ? 'block' : 'none' }}>
+              <img src={img2} alt="Oferta 2" width="100%" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul className={style.servicios}>
+        <li className={style.servicioItem}>
+          <BiSolidCreditCardFront className={style.icon} />{' '}
+          <span>Hasta 12 cuotas sin interés</span>
+        </li>
+        <li className={style.servicioItem}>
+          <FaWhatsapp className={style.icon} /> <span>Atención personalizada</span>
+        </li>
+        <li className={style.servicioItem}>
+          <FaTruck className={style.icon} /> <span>Calculá el costo de tu envío</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
