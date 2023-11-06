9109c4e9a2871da2acb51b4c3867406096607a35
JuanJustoMuller <juancitomuller@gmail.com>
2023-11-05 23:24:39 -0300

    Correccion boton finalizar compra, queda un bug en la redireccion a mercado pago todavia

--- Client/src/views/Cart/Cart.jsx
+++ Client/src/views/Cart/Cart.jsx
@@ -11,36 +11,29 @@ const Cart = () => {
   // Obtengo los productos y la cantidad del estado
   const items = useSelector((state) => state.search.cart);
   const dispatch = useDispatch();
-
-  const [preferenceId, setpreferenceId] = useState(null)
-  initMercadoPago("TEST-4443090684227901-110316-34c0605bc0b2477e1bcdcc56b42939ff-223028779");
+  const [preferenceId, setPreferenceId] = useState(null)
+  console.log("prefrerenceIDD:", preferenceId);
+  initMercadoPago("TEST-dfa942a8-d4d9-444f-924a-0060dff9b2d3");
 
 const createPreference = async () => {
     try {
       const response = await axios.post("http://localhost:3001/order", //peticion post, envia algo al back (al servidor, localhost)
-        items[0]
-      );
-
-      const {id} = response.data; //recibe un id, que viene de response.data(del servidor)
+        items[0]);
+      console.log("la response del front:", response)
+      const {id} = response.data;
+      console.log(id); //recibe un id, que viene de response.data(del servidor)
       return id;                  //retorna ese id para utilizarlo dsp
     } catch (error) {
       console.log(error);
     }
-  }
-
-  /*const createPreference = () => {
-    axios
-    .post("http://localhost:3001/order", {...Cart, items})
-    .then((response) => {
-      windows.location.href = response.data.init_point
-    })
-    .catch(error => console.log(error)) 
-  }*/
+  };
 
   const handleBuy = async () => {      //funcion asincrona al hacer click en comprar
-    const id = await createPreference(); //recibe lo de createPreference q seria un id en definitiva
+    const id = await createPreference();
+    console.log("id del handlebuy: ", id); //recibe lo de createPreference q seria un id en definitiva
     if (id) {
-      setpreferenceId(id)
+      setPreferenceId(id)
+      console.log("setpreferenceId:", id);
     }
   }
 
@@ -98,7 +91,8 @@ useEffect(() => {
 
       <div>
         <button onClick={handleBuy}>FINALIZAR COMPRA</button>
-        {preferenceId && <Wallet initializacion = {preferenceId} />}
+        {preferenceId && <Wallet initialization={preferenceId} />} 
+        
       </div>
 
     </div>
@@ -106,3 +100,5 @@ useEffect(() => {
 };
 
 export default Cart;
+
+//<Wallet initialization={{ preferenceId: '<PREFERENCE_ID>' }} />
