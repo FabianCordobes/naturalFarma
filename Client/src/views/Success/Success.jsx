5047f1ca41b28754344747b94302868fe4b1ec84
fabiancord <fabianarielcordobes@gmail.com>
2023-11-14 10:09:35 -0300

    success componente para guardar y enviar el ususario

--- Client/src/views/Success/Success.jsx
+++ Client/src/views/Success/Success.jsx
@@ -1,28 +1,29 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react'; // Importa useState
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const Success = () => {
 	const location = useLocation();
-	const searchParams = location.search.substring(1); // Elimina el '?' inicial de la cadena
-	const paramsArray = searchParams.split('&'); // Divide en pares de clave-valor
-	const paramsObject = {};
-	const { user } = useAuth0();
-
+	const [orderData, setOrderData] = useState({
+		products: [],
+		orderDetail: {},
+		user: [], // Agrega la propiedad user al objeto
+	}); // Usa useState para manejar el estado
 	const storedCart = localStorage.getItem('cart');
+	const userID = localStorage.getItem('user');
+	const parsedID = userID.split('"').join('');
 
+	const paramsArray = location.search.substring(1).split('&');
+	const paramsObject = {};
 	paramsArray.forEach((param) => {
 		const [key, value] = param.split('=');
 		paramsObject[key] = value;
 	});
 
-	// Extraer solo las propiedades necesarias de paramsObject
 	const { payment_id, status, merchant_order_id, payment_type, preference_id } =
 		paramsObject;
 
-	// Crear un nuevo objeto orderDetail con las propiedades necesarias
 	const orderDetail = {
 		payment_id,
 		status,
@@ -31,26 +32,27 @@ const Success = () => {
 		preference_id,
 	};
 
-	if (storedCart) {
-		let parsedData = JSON.parse(storedCart);
+	const fetchData = async () => {
+		try {
+			// Realiza la solicitud GET del usuario
+			const response = await axios.get(`/user/${parsedID}`);
+			const userData = response.data;
 
-		var orderData = {
-			products: parsedData,
-			orderDetail: orderDetail,
-		};
-
-		// console.log(orderData);
-	}
-
-	// console.log(orderData.push(paramsObject));
+			// Actualiza el objeto orderData con la información del usuario
+			setOrderData({
+				products: JSON.parse(storedCart),
+				orderDetail: orderDetail,
+				user: userData[0], // Agrega la propiedad user al objeto
+			});
+		} catch (error) {
+			console.error('Error fetching user data:', error.message);
+		}
+	};
 
 	useEffect(() => {
-		// const response = axios.get('/order', orderData)
-		// console.log(paramsObject);
-		if (user) {
-			console.log(user);
-		}
-	}, [location.href]);
+		fetchData(); // Llama a la función de solicitud cuando el componente se monta
+		console.log(orderData);
+	}, [parsedID]); // Añade las dependencias al array de dependencias
 
 	return <div>{storedCart}</div>;
 };
