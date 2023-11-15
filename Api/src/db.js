d25afb2b43dfcb13e5ca1e91c5644fc099113616
fabiancord <fabianarielcordobes@gmail.com>
2023-11-15 14:23:55 -0300

    intentando deployar back

--- Api/src/db.js
+++ Api/src/db.js
@@ -8,7 +8,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`${DB_URL}`, {
   logging: false, 
   native: false, 
 });
