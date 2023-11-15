dcfd179bf37771429b392acaa98a13dddc420faf
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2023-11-15 18:44:56 -0300

    cambios sobre el mail

--- Api/config/nodemaler.js
+++ Api/config/nodemaler.js
@@ -2,10 +2,10 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-  host:"smtp.gmail.coms",
+  host:"smtp.gmail.com",
   port: 465,
   secure: true,
-  service: 'Gmail',
+  service: 'gmail',
   auth: {
     user: 'andreaochoaispuru@gmail.com', 
     pass: 'vwyb uzjh hwsy nets'
@@ -14,19 +14,20 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify().then(()=> console.log("gmail enviado con exito!!")).catch((error) => console.log(error));
 
-const enviarCorreo = async (toEmail, name) => {
-    try {
-      await transporter.sendMail({
-        from: 'andreaochoaispuru@gmail.com',
-        to: toEmail,
-        subject: 'Compra realizada en Mercado Pago',
-        text: 'Se ha realizado una compra en Mercado Pago.',
-        html: `<h1>${name} tu compra se realizó con éxito</h1>`
-      });
-      console.log('Correo enviado');
-    } catch (error) {
-      console.error('Error al enviar el correo:', error);
-    }
-  };
+// const enviarCorreo = async (toEmail, name) => {
+//     try {
+//       await transporter.sendMail({
+//         from: 'andreaochoaispuru@gmail.com',
+//         to: toEmail,
+//         subject: 'Compra realizada en Mercado Pago',
+//         text: 'Se ha realizado una compra en Mercado Pago.',
+//         html: `<h1>${name} tu compra se realizó con éxito</h1>`
+//       });
+//       console.log('Correo enviado');
+//     } catch (error) {
+//       console.error('Error al enviar el correo:', error);
+//     }
+//   };
 
-module.exports = {transporter, enviarCorreo}
+module.exports = transporter;
+// enviarCorreo
--- Api/src/controllers/PaymentController.js
+++ Api/src/controllers/PaymentController.js
@@ -1,53 +1,57 @@
 const { MercadoPagoConfig, Preference } = require('mercadopago');
 const { ACCESS_TOKEN } = process.env;
-const {History} = require ("../db");
-const  {enviarCorreo}  = require ("../../config/nodemaler")
+const { History } = require("../db");
+// const { enviarCorreo } = require("../../config/nodemaler");
 
 const client = new MercadoPagoConfig({
-	accessToken: ACCESS_TOKEN,
+  accessToken: ACCESS_TOKEN,
 });
 const payment = new Preference(client);
 
-//genera orden de compra a mercado pago con la info que llega por body
 // Genera orden de compra en Mercado Pago con la información que llega por el cuerpo de la solicitud
 const createOrder = async (items) => {
-	// Construye el array de items para la preferencia a partir de los productos recibidos
-	const products = [];
+  // Construye el array de items para la preferencia a partir de los productos recibidos
+  const products = [];
+  
+   items.map((item) => ({
+    id: item.id,
+    title: item.brand,
+    unit_price: item.price,
+    quantity: item.quantity,
+    picture_url: item.image,
+    currency_id: 'ARG',
+  }));
 
-	items.map((item) =>
-		products.push({
-			id: item.id,
-			title: item.brand,
-			unit_price: item.price,
-			quantity: item.quantity,
-			picture_url: item.image,
-			currency_id: 'ARG',
-		})
-	);
-	console.log(products);
-	let preference = {
-		body: {
-			items: products,
+  console.log("Aca van los productos "+products);
 
-			back_urls: {
-				failure: 'https://natural-farma.onrender.com/cart',
-				pending: '',
-				success: 'http://localhost:5173/success',
-			},
-			auto_return: 'approved',
-		},
-	};
+  let preference = {
+    body: {
+      items: products,
 
-	console.log(items);
-	const response = await payment.create(preference);
-	return response;
+      back_urls: {
+        failure: 'https://natural-farma.onrender.com/cart',
+        pending: '',
+        success: 'http://localhost:5173/success',
+      },
+      auto_return: 'approved',
+    },
+  };
+
+  console.log("ACa los intems" +items);
+
+  const response = await payment.create(preference);
+
+  await getSuccesfulPurchase(items);
+
+  return response;
 };
+
 const getSuccesfulPurchase = async (items) => {
-		await History.create({ items, status: 'successful' });
-		await enviarCorreo(items[0].email, items[0].name);
+  await History.create({ items, status: 'successful' });
+  await enviarCorreo(items[0].email, items[0].name);
 };
 
 module.exports = {
-	createOrder,
-	getSuccesfulPurchase,
+  createOrder,
+  getSuccesfulPurchase,
 };
